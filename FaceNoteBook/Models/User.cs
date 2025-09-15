1541cce7299d76a6ff0687654f21c01e619b6279
teneieiza <tenmonsterten@gmail.com>
2025-09-15 16:43:55 +0700

    Add UserModel

---
+++ FaceNoteBook/Models/User.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FaceNoteBook.Models;
+
+public class User
+{
+  public Guid Id { get; set; }
+
+  [Required]
+  [StringLength(50)]
+  public string Name { get; set; } = string.Empty;
+
+  [Required]
+  [StringLength(50)]
+  [EmailAddress]
+  public string Email { get; set; } = string.Empty;
+  
+  [Required]
+  [StringLength(150)]
+  public string Password { get; set; } = string.Empty;
+  
+  public DateTime CreatedAt { get; set; }
+  public DateTime UpdatedAt { get; set; }
+}
