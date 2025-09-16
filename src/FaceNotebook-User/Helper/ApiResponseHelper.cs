30427d3ff4f1ff3eff2ca4365312d9b539c881cc
teneieiza <tenmonsterten@gmail.com>
2025-09-16 16:23:06 +0700

    Create Helper for use format response

---
+++ src/FaceNotebook-User/Helper/ApiResponseHelper.cs
@@ -0,0 +1,16 @@
+using FaceNoteBook.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FaceNoteBook.Helper
+{
+    public static class ApiResponseHelper
+    {
+        public static ActionResult<ApiResponse<T>> Success<T>(T data, int statusCode = 200, string message = "OK")
+        {
+            return new ObjectResult(new ApiResponse<T>(statusCode, message, data))
+            {
+                StatusCode = statusCode
+            };
+        }
+    }
+}
