54e35e24a6cd0cb337e42a2150da4f2b92f1edc4
teneieiza <tenmonsterten@gmail.com>
2025-09-16 16:25:48 +0700

    Change folder and split method PUT Dto

--- src/FaceNotebook-User/DTOs/UsersDto.cs
+++ src/FaceNotebook-User/DTOs/UsersDto.cs
@@ -18,17 +18,20 @@ public class CreateUserDto
     public string Password { get; set; } = string.Empty;
 }
 
-public class UpdateUserDto
+public class UpdateDetailDto
 {
-    [StringLength(50)]
-    public string? Name { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
 
-    [StringLength(50)]
-    [EmailAddress]
-    public string? Email { get; set; }
+public class UpdateEmailDto
+{
+    public string Email { get; set; } = string.Empty;
+}
 
-    [StringLength(150)]
-    public string? Password { get; set; }
+public class UpdatePasswordDto
+{
+    public string OldPassword { get; set; } = string.Empty;
+    public string NewPassword { get; set; } = string.Empty;
 }
 
 public class UserLoginDto
@@ -49,3 +52,17 @@ public class UserResponseDto
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 }
+
+    public class ApiResponse<T>
+    {
+        public int Status { get; set; }
+        public string Message { get; set; }
+        public T? Data { get; set; }
+
+        public ApiResponse(int status, string message, T? data = default)
+        {
+            Status = status;
+            Message = message;
+            Data = data;
+        }
+    }
