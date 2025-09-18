ca2265010125b4cfa35fdd02b207672fe32b9489
teneieiza <tenmonsterten@gmail.com>
2025-09-18 21:59:11 +0700

    Edit Usermodel to use RefreshToken & RefreshTokenExpiryTime

--- src/FaceNotebook-User/Models/User.cs
+++ src/FaceNotebook-User/Models/User.cs
@@ -14,11 +14,15 @@ public class User
   [StringLength(50)]
   [EmailAddress]
   public string Email { get; set; } = string.Empty;
-  
+
   [Required]
   [StringLength(150)]
   public string Password { get; set; } = string.Empty;
-  
+
   public DateTime CreatedAt { get; set; }
   public DateTime UpdatedAt { get; set; }
+  
+  public string? RefreshToken { get; set; }
+  public DateTime? RefreshTokenExpiryTime { get; set; }
+
 }
