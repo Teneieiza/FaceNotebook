cffc3dc5967720f2ad21b8a5d28741b9cdf5ce3c
teneieiza <tenmonsterten@gmail.com>
2025-09-18 22:00:51 +0700

    Add Fail to ApiResponseHelper

--- src/FaceNotebook-User/Utils/ApiResponseHelper.cs
+++ src/FaceNotebook-User/Utils/ApiResponseHelper.cs
@@ -5,12 +5,21 @@ namespace FaceNoteBook.Utils
 {
     public static class ApiResponseHelper
     {
-        public static ActionResult<ApiResponse<T>> Success<T>(T data, int statusCode = 200, string message = "OK")
+        public static ActionResult<ApiResponse<T>> Success<T>(T? data = default, int statusCode = 200, string message = "OK")
         {
             return new ObjectResult(new ApiResponse<T>(statusCode, message, data))
             {
                 StatusCode = statusCode
             };
         }
+
+        public static ActionResult<ApiResponse<object>> Fail(string message, int statusCode = 400)
+        {
+            return new ObjectResult(new ApiResponse<object>(statusCode, message))
+            {
+                StatusCode = statusCode
+            };
+        }
     }
+
 }
